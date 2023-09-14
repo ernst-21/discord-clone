ad0927179cf8fa896ed6c70001afaf062fc1cd65
Ernesto Rivera <riverag8504@gmail.com>
2023-09-13 22:01:01 -0400

    [feature] created server modal

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -1,12 +1,17 @@
 'use client';
 import ActionToolTip from '@/components/action-tooltip';
+import { useModal } from '@/hooks/use-modal-store';
 import { Plus } from 'lucide-react';
 
 const NavigationAction = () => {
+	const { onOpen } = useModal();
 	return (
 		<div>
 			<ActionToolTip label='Add a server' align='center' side='right'>
-				<button className='group flex items-center'>
+				<button
+					onClick={() => onOpen('createServer')}
+					className='group flex items-center'
+				>
 					<div className='flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500'>
 						<Plus
 							className='group-hover:text-white transition-all text-emerald-500'
